715aa474914b8b35a4e65fddbd4746f83bf9ffdd
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2019-02-26 18:12:00 +0000

    Add opposing account to import statement

--- silverstrike/importers/import_statement.py
+++ silverstrike/importers/import_statement.py
@@ -1,12 +1,17 @@
 class ImportStatement(object):
     account = ''
+    opposing_account = ''
     book_date = ''
     transaction_date = ''
     amount = 0
     notes = ''
-    iban = ''
+    account_iban = ''
+    opposing_account_iban = ''
 
     def __init__(self, **kwargs):
         for name, value in kwargs.items():
             setattr(self, name, value)
         self.transaction_date = self.transaction_date or self.book_date
+
+    def __str__(self):
+        return self.account_iban+" "+self.opposing_account_iban+" "+self.notes+" "+str(self.amount)
