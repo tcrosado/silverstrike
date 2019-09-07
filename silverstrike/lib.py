1f2fb83b3f28f7c1e950c7ead95fec5cff87e2a7
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2019-09-08 00:08:54 +0100

    Update to Python3.6 because of yfinance and add price fetcher

--- silverstrike/lib.py
+++ silverstrike/lib.py
@@ -1,6 +1,28 @@
 import datetime
 
+from silverstrike.models import SecurityPrice
+import yfinance as yf
 
 def last_day_of_month(any_day):
     next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
     return next_month - datetime.timedelta(days=next_month.day)
+
+def update_security_price(ticker,start=None,end=None):
+    most_recent = SecurityPrice.objects.order_by('date').first()
+    if most_recent != None:
+        if most_recent.date == datetime.datetime.now():
+            return True
+        
+        next_date = most_recent.date + datetime.timedelta(days=1)
+        data_list = yf.download(ticker, start=next_date.strftime('%Y-%m-%d'))['Close']
+    else:
+        data_list = yf.download(ticker)['Close']
+
+    for i in range(len(data_list)):
+        date = data_list.index[i]
+        price = data_list[i]
+        SecurityPrice()
+
+        SecurityPrice.objects.create(ticker=ticker, date=date, price=price)
+
+    return True
