22fb5ec212bcd027113bff66949326b4250df31e
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2019-12-31 00:46:09 +0000

    Remove prints and add comments TODO

--- silverstrike/utils/InvestmentCalculator.py
+++ silverstrike/utils/InvestmentCalculator.py
@@ -4,7 +4,6 @@ from silverstrike.models import SecurityDetails, SecurityTypeTarget, SecurityReg
     SecurityDistribution, SecurityBondMaturity
 from silverstrike.utils.AssetTypeWeightCalculator import AssetTypeWeightCalculator
 from silverstrike.utils.BondMaturityWeightCalculator import BondMaturityWeightCalculator
-from silverstrike.utils.InvestmentWeightCalculator import InvestmentWeightCalculator
 from silverstrike.utils.PriceGetter import PriceGetter
 from silverstrike.utils.RegionDistributionWeightCalculator import RegionDistributionWeightCalculator
 from silverstrike.utils.SecurityQuantityGetter import SecurityQuantityGetter
@@ -24,21 +23,18 @@ class InvestmentCalculator:
         added_amount = 0
         updated = True
 
-        #Set current security quantities
+        # Set current security quantities
         self.security_quantity_getter.clear_list()
         current_security_quantity = SecurityQuantityGetter().get_quantities()
         for isin in current_security_quantity.keys():
-            self.security_quantity_getter.set_security_quantity(isin,current_security_quantity[isin])
+            self.security_quantity_getter.set_security_quantity(isin, current_security_quantity[isin])
 
         while added_amount != amount and updated:
-            print(self.security_quantity_getter.get_quantities())
             updated = False
             # select asset to add
             delta_asset_weights = self.__get_delta_asset_weights()
-            print(delta_asset_weights)
             selected_asset = min(delta_asset_weights, key=delta_asset_weights.get)
-            print(selected_asset)
-
+            # FIXME refactor asset types
             if selected_asset == SecurityDetails.STOCK:
                 # get delta_region_target
                 delta_region_weights = self.__get_delta_region_weights()
@@ -51,7 +47,7 @@ class InvestmentCalculator:
                 selected_isin = max(region_weight_list, key=operator.attrgetter('allocation')).isin
 
                 # get price
-                #FIXME if no price avaliable
+                # FIXME if no price avaliable
                 price = float(self.price_getter.get_latest_prices([selected_isin])[selected_isin])
 
                 if amount >= price:
@@ -70,7 +66,8 @@ class InvestmentCalculator:
                 # get bond with max maturity
                 bond_list = SecurityDetails.objects.filter(security_type=SecurityDetails.BOND)
                 isin_list = [bond.isin for bond in bond_list]
-                maturity_weight_list = SecurityBondMaturity.objects.filter(maturity_id=selected_maturity, isin__in=isin_list)
+                maturity_weight_list = SecurityBondMaturity.objects.filter(maturity_id=selected_maturity,
+                                                                           isin__in=isin_list)
                 selected_allocaion = max(maturity_weight_list, key=operator.attrgetter('allocation'))
                 selected_isin = selected_allocaion.isin
                 # get price
@@ -85,7 +82,8 @@ class InvestmentCalculator:
 
             elif selected_asset == SecurityDetails.REIT:
                 # add reit asset
-                #FIXME if more than 1 REIT
+                # FIXME if more than 1 REIT
+                # TODO option to not consider certain securities to be bought in the future
                 selected_security = SecurityDetails.objects.filter(security_type=SecurityDetails.REIT)[0]
                 selected_isin = selected_security.isin
                 price = float(self.price_getter.get_latest_prices([selected_isin])[selected_isin])
@@ -99,17 +97,12 @@ class InvestmentCalculator:
             else:
                 raise Exception("Unknown asset selected")
 
-
-        #TODO Test
-        print("End Cal")
         return self.security_quantity_getter.get_quantities()
 
-    #TODO refactor deltas
+    # TODO refactor deltas
     def __get_delta_asset_weights(self):
         asset_weights = self.asset_weight_calculator.calculate_weights()
-        print("Asset Weights")
-        print(asset_weights)
-        asset_targets = SecurityTypeTarget.objects.all() #FIXME single user
+        asset_targets = SecurityTypeTarget.objects.all()  # FIXME single user
         delta_asset_weigths = dict()
 
         for target in asset_targets:
@@ -138,8 +131,10 @@ class InvestmentCalculator:
 
         return delta_maturity_weights
 
-    def sell(self,amount):
-        return None
+    def sell(self, amount):
+        # TODO
+        raise NotImplementedError
 
     def rebalance(self):
-        return None
+        # TODO
+        raise NotImplementedError
